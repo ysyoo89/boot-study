17e23355c1fc6fb9b1a306a55ec161f88947e175
ysyoo89 <ysyoo89@kakao.com>
2022-11-13 23:42:33 +0900

    [스프링 데이터] 인메모리 데이터베이스

--- src/main/java/com/example/bootstudy/H2Runner.java
+++ src/main/java/com/example/bootstudy/H2Runner.java
@@ -3,6 +3,7 @@ package com.example.bootstudy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -14,6 +15,9 @@ public class H2Runner implements ApplicationRunner {
 
     @Autowired
     DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -29,6 +33,13 @@ public class H2Runner implements ApplicationRunner {
             String sql = "CREATE TABLE TEST_USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
             statement.executeUpdate(sql);
         }
+
+        /**
+         * JdbcTemplate
+         * Connection보다 좀 더 안전하게 사용가능하다.
+         * 예외를 던질때 좀 더 가독성이 좋다.
+         */
+        jdbcTemplate.execute("INSERT INTO TEST_USER VALUES (1, 'yulseon')");
 
 
     }
